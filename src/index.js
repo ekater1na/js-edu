5c1051d19228ecb5a2170fabbea2dacf8a2a81cd
ekater1na <ekaterina_l@tut.by>
2019-09-27 15:12:11 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
+  
+
+  module.exports = function getTimeForEducation(
     focus = 'family', 
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      let focusConfig = config[focus];
+      let progTime = knowsProgramming ? 800 : 1300
+      return Math.ceil(progTime/focusConfig);
   };
-  
+
+    
